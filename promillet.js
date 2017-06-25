8b03f6aa697895077979320b4c8daa0a3d7487d0
Joonas Ulmanen <joonas.ulmanen@futurice.com>
2017-06-25 19:52:15 +0300

    Fix

--- promillet.js
+++ promillet.js
@@ -204,13 +204,12 @@ function makeDrinksString(drinks) {
   for(var i in drinks) {
     let drink = drinks[i];
     let drinkTime = new Date(Date.parse(drink.created));
-    let drinkShortDate = drinkTime.getDay() + '.' + (drinkTime.getMonth()+1);
+    let drinkShortDate = drinkTime.getDate() + '.' + (drinkTime.getMonth()+1);
     if(day !== drinkShortDate) {
       day = drinkShortDate;
       list.push(day);
     }
-    list.push(drink.description);
-    console.log(list);
+    list.push(drink.description + ' ' + drinkTime.getHours() + ':' + drinkTime.getMinutes());
   }
   return list.join('\n');
 }
