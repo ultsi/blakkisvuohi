6304161d55e63fe8ccd4f398eca33c582c440ce7
Joonas Ulmanen <joonas.ulmanen@futurice.com>
2017-06-25 13:24:15 +0300

    debug

--- promillet.js
+++ promillet.js
@@ -5,6 +5,7 @@ const utils = require('./utils.js');
 const users = require('./users.js');
 
 cmd.register('/luotunnus', cmd.TYPE_PRIVATE, function(msg, words){
+  console.log(words);
   users.new(msg.from.id, msg.from.username, words[1], words[2])
   .then(function(user){
     utils.sendPrivateMsg(msg, 'Moikka ' + user.nick);
