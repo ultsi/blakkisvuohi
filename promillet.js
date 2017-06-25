dacc83ec13c107b2890a64fe7bfab659618d0b89
Joonas Ulmanen <joonas.ulmanen@futurice.com>
2017-06-25 13:19:58 +0300

    Fix features

--- promillet.js
+++ promillet.js
@@ -2,7 +2,13 @@
 
 const cmd = require('./cmd.js');
 const utils = require('./utils.js');
+const users = require('./users.js');
 
 cmd.register('/luotunnus', cmd.TYPE_PRIVATE, function(msg, words){
-  utils.sendPrivateMsg(msg, 'luotu!');
-}, 'Komennon käyttö: /luotunnus <paino> <mies/nainen>');
+  users.new(msg.from.id, msg.from.username, words[1], words[2])
+  .then(function(user){
+    utils.sendPrivateMsg(msg, 'Moikka ' + user.nick);
+  }, function(err){
+    utils.sendPrivateMsg(msg, err);
+  });
+}, '/luotunnus <paino> <mies/nainen>');
