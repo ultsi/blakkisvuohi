9b114ca86f1cccd5c118cca69e800af54f0f08ae
Joonas Ulmanen <joonas.ulmanen@futurice.com>
2017-07-02 00:32:15 +0300

    Fix

--- promillet.js
+++ promillet.js
@@ -184,7 +184,7 @@ cmd.registerUserCmd('/promillet', cmd.TYPE_ALL, function(msg, words, user){
               permilles.push([user.nick, getPermilles(user, grams), drinkCountsByUser[details.userid].count]);
             }
           }
-          permilles = permilles.sort(user => user[1]).map(user => user[0] + '... ' + user[1].toFixed(2) + '‰ ('+user[2]+')');
+          permilles = permilles.sort(user => -user[1]).map(user => user[0] + '... ' + user[1].toFixed(2) + '‰ ('+user[2]+')');
           deferred.resolve(cmd.chatResponse(msg.chat.title + ' -kavereiden rippitaso:\nKäyttäjä...‰ (annokset/24h)\n\n' + permilles.join('\n')));
         } catch (err) {
           console.error(err);
