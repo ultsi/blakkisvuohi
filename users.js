3872350bfc825a075fd9c9f535041cf4274bb38c
Joonas Ulmanen <joonas.ulmanen@gmail.com>
2017-10-26 22:55:25 +0300

    Fix brainfart

--- users.js
+++ users.js
@@ -253,7 +253,7 @@ User.prototype.getDrinkCountsByGroupsForUser = function() {
 
 users.getBoozeByHourForGroup = function(groupId) {
   let deferred = when.defer();
-  query('select users.userId, users.nick, users.weight, users.gender, (sum(alcohol) OVER (ORDER BY date_part('hour', created))) as sum, date_part(\'hour\', created) as hr from users_drinks join users_in_groups on users_drinks.userid=users_in_groups.userid join users on users.userid=users_drinks.userid where groupid=$1 and created >= NOW() - \'1 day\'::INTERVAL group by hr, users.userid, users.nick, users.weight, users.gender, users_drinks.alcohol',[groupId])
+  query('select users.userId, users.nick, users.weight, users.gender, (sum(alcohol) OVER (ORDER BY date_part(\'hour\', created))) as sum, date_part(\'hour\', created) as hr from users_drinks join users_in_groups on users_drinks.userid=users_in_groups.userid join users on users.userid=users_drinks.userid where groupid=$1 and created >= NOW() - \'1 day\'::INTERVAL group by hr, users.userid, users.nick, users.weight, users.gender, users_drinks.alcohol',[groupId])
     .then(function(res){
       let drinksByUser = res[0];
       deferred.resolve(res);
