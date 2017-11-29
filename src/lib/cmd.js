8e9a977d1c0b067b12c4be4437475f2bed607d4b
Joonas Ulmanen <joonas.ulmanen@gmail.com>
2017-11-29 19:18:58 +0200

    Prepend all files with license text

--- src/lib/cmd.js
+++ src/lib/cmd.js
@@ -1,4 +1,22 @@
 /*
+    Bläkkisvuohi, a Telegram bot to help track estimated blood alcohol concentration.
+    Copyright (C) 2017  Joonas Ulmanen
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
  cmd.js
  Simple command library to register different commands and call them via callbacks.
  Dependent on context and the user framework that Bläkkisvuohi uses.
