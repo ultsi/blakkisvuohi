6d7913a5bd5e9fd269e7c7c71c574d62ddf199b3
Joonas Ulmanen <joonas.ulmanen@gmail.com>
2017-12-23 11:46:52 +0200

    Add alcohol equations

--- app/lib/alcomath.js
+++ app/lib/alcomath.js
@@ -26,6 +26,51 @@
 const constants = require('../constants.js');
 let alcomath = module.exports = {};
 
+/* Variation of Widmark equation as developed by
+    National Highway Traffic Safety Administration, 1994
+    (https://academic.oup.com/alcalc/article/40/5/447/188561#1793285)
+    (https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2724514/#B15)
+
+    Are the calculations correct? https://doi.org/10.15288/jsa.2002.63.762
+
+    EBAC = ((0.806 * SD * 1.2) / (BW * Wt) - (MR * DP)) * 10
+
+    where,  0.806 is a constant for body water in the blood (mean 80.6%),
+            SD is the number of standard drinks (10g ethanol per drink)
+            1.2 to convert to Swedish unit of alcohol (alkoholiannos)
+            BW is the body water constant (0.58 for males, 0.49 for females)
+            Wt is the body weight (kg)
+            MR is the metabolism rate (0.015 for males, 0.017 for females)
+            DP is the drinking period (hours)
+
+    Example:
+
+    Ethanol grams per litre = 0.789g/cm^3
+    Ethanol grams in a serving 
+
+    Pure alcohol mass = volume * (alcohol by volume * volumetric mass density)
+    
+    where,  volume (litres)
+            alcohol by volume = reported vol percentage (for example 4.7%)
+            volumetric mass density = 789.24g/l
+
+    Two 0.33l 4.7% beers
+
+    g = 2 * 0.33l * 0.047 * 789.24g/l
+    g = 24.48g
+
+    What is the EBAC if 80kg male consumes 2 beers in 1 hour?
+    
+    SD = 24.48g/10g = 2.45
+    BW = 0.58
+    Wt = 80kg
+    MR = 0.017
+    DP = 1
+    EBAC = ((0.806 * 2.45 * 1.2) / (0.58 * 80) - (0.015*1)) * 10
+         = 0.36 permilles
+    
+*/
+
 alcomath.getPermillesFromGrams = function(user, grams) {
     let standard_drinks = grams / constants.STANDARD_DRINK_GRAMS;
     return (constants.MEAN_BODY_WATER * (standard_drinks)) / (constants.LIQUID_PERCENT[user.gender] * user.weight) * 10;
