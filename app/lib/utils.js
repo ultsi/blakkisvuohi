843e7a293856c7132a336f48358499e3bbf85d13
Joonas Ulmanen <joonas.ulmanen@gmail.com>
2018-01-13 13:48:59 +0200

    Add tests for utils.js

--- app/lib/utils.js
+++ app/lib/utils.js
@@ -125,7 +125,7 @@ utils.attachMethods = function attachMethods(msg, bot) {
 };
 
 utils.roundTo = (n, t) => {
-    t = t ||  0;
+    t = t || 0;
     return Math.round(n * (Math.pow(10, t))) / Math.pow(10, t);
 };
 
@@ -134,11 +134,15 @@ utils.getRandom = function(arr) {
 };
 
 utils.isValidInt = function(num) {
-    return !!parseInt(num, 10);
+    const n = Number(num);
+    const int = parseInt(num, 10);
+    return !isNaN(int) && (typeof int === 'number' || int instanceof Number) && int === n;
 };
 
 utils.isValidFloat = function(num) {
-    return !!parseFloat(num);
+    const n = Number(num);
+    const float = parseFloat(num);
+    return !isNaN(float) && (typeof float === 'number' || float instanceof Number) && float === n;
 };
 
 /*
@@ -146,22 +150,22 @@ utils.isValidFloat = function(num) {
     http://alumni.media.mit.edu/~wad/color/numbers.html
 */
 const wadsPalette = [
-    [0, 0, 0], // Black 
-    [87, 87, 87], // Dk. Gray 
-    [173, 35, 35], // Red 
-    [42, 75, 215], // Blue 
-    [29, 105, 20], // Green 
-    [129, 74, 25], // Brown 
-    [129, 38, 192], // Purple 
-    [160, 160, 160], // Lt. Gray 
-    [129, 197, 122], // Lt. Green 
-    [157, 175, 255], // Lt. Blue 
-    [41, 208, 208], // Cyan 
-    [255, 146, 51], // Orange 
-    [255, 238, 51], // Yellow 
-    [233, 222, 187], // Tan 
-    [255, 205, 243] // Pink 
-    // [255, 255, 255] // White 
+    [0, 0, 0], // Black
+    [87, 87, 87], // Dk. Gray
+    [173, 35, 35], // Red
+    [42, 75, 215], // Blue
+    [29, 105, 20], // Green
+    [129, 74, 25], // Brown
+    [129, 38, 192], // Purple
+    [160, 160, 160], // Lt. Gray
+    [129, 197, 122], // Lt. Green
+    [157, 175, 255], // Lt. Blue
+    [41, 208, 208], // Cyan
+    [255, 146, 51], // Orange
+    [255, 238, 51], // Yellow
+    [233, 222, 187], // Tan
+    [255, 205, 243] // Pink
+    // [255, 255, 255] // White
 ];
 
 utils.getColorSet = function() {
