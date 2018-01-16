830314aaffdc48fb3b9dbee8e5fd5a738eaa7e9f
Joonas Ulmanen <joonas.ulmanen@gmail.com>
2018-01-16 22:06:26 +0200

    Add commands.js tests

--- test/lib/commands.js
+++ test/lib/commands.js
@@ -21,7 +21,74 @@
     unit tests for commands.js functions
 */
 
-/* globals describe, it */
+/* globals describe, it, beforeEach */
 
 'use strict';
+
+const assert = require('assert');
+const blakkistest = require('../blakkistest.js');
+const mocked = blakkistest.mockMsgAndBot();
 const Commands = require('../../app/lib/commands.js');
+const Message = require('../../app/lib/message.js');
+
+const testCommand = {
+    [0]: {
+        startMessage: Message.PrivateMessage('test'),
+        validateInput: () => {
+            return true;
+        },
+        onValidInput: (context) => {
+            return context.end();
+        }
+    }
+};
+
+describe('commands.js', function() {
+    beforeEach(blakkistest.resetDbWithTestUsersAndGroupsAndDrinks);
+    describe('Commands.register()', function() {
+        it('should register a command that can be called', function() {
+            let msg = Object.assign(mocked.msg); // copy
+            Commands.register('/test', 'help', Commands.TYPE_ALL, testCommand);
+            Commands.call('/test', msg, ['/test']);
+            assert.equal(mocked.internals.sentChatId, mocked.msg.from.id);
+            assert.equal(mocked.internals.sentText, 'test');
+        });
+    });
+
+    describe('Commands.registerUserCommand()', function() {
+        it('should register a command that can be called by a valid user', function(done) {
+            let msg = Object.assign(mocked.msg); // copy
+            msg.from.id = blakkistest.realIds[0];
+            Commands.registerUserCommand('/testusercommand', 'help', Commands.TYPE_ALL, [() => {
+                done();
+                return Promise.resolve();
+            }]);
+            Commands.call('/testusercommand', msg, ['/testusercommand']);
+        });
+
+        it('a user command should not be able to be called by a non-user', function(done) {
+            this.slow(200);
+            let msg = Object.assign(mocked.msg); // copy
+            msg.from.id = 0; // non user id
+            Commands.registerUserCommand('/testusercommand2', 'help', Commands.TYPE_ALL, [() => {
+                done(new Error('non user used a user command'));
+                return Promise.resolve();
+            }]);
+            Commands.call('/testusercommand2', msg, ['/testusercommand2']);
+            setTimeout(() => done(), 50);
+        });
+
+        it('a private user command should not be able to be called in a chat', function(done) {
+            this.slow(200);
+            let msg = Object.assign(mocked.msg); // copy
+            msg.from.id = blakkistest.realIds[0];
+            msg.chat.type = 'chat';
+            Commands.registerUserCommand('/testusercommand2', 'help', Commands.TYPE_PRIVATE, [() => {
+                done(new Error('private command used in chat'));
+                return Promise.resolve();
+            }]);
+            Commands.call('/testusercommand2', msg, ['/testusercommand2']);
+            setTimeout(() => done(), 50);
+        });
+    });
+});
