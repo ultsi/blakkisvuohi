2c80a35d4ea06fe76064162972e39172c3450aab
Joonas Ulmanen <joonas.ulmanen@gmail.com>
2018-01-16 14:26:40 +0200

    Add rest of users.js tests

--- test/db/users.js
+++ test/db/users.js
@@ -56,7 +56,7 @@ describe('users.js', function() {
                     try {
                         assert.equal(user.username, 'nick');
                     } catch (err) {
-                        done(err);
+                        return done(err);
                     }
                     return query('select * from users');
                 })
@@ -65,7 +65,7 @@ describe('users.js', function() {
                         assert.notEqual(res[0].find(x => x.userid === utils.hashSha256('1')), undefined);
                         done();
                     } catch (err) {
-                        done(err);
+                        return done(err);
                     }
                 })
                 .catch((err) => {
@@ -85,7 +85,7 @@ describe('users.js', function() {
                         assert.equal(found.nick, utils.encrypt('nick'));
                         done();
                     } catch (err) {
-                        done(err);
+                        return done(err);
                     }
                 })
                 .catch((err) => {
@@ -101,7 +101,7 @@ describe('users.js', function() {
                     try {
                         assert.equal(user.username, 'nick');
                     } catch (err) {
-                        done(err);
+                        return done(err);
                     }
                     return users.find('1');
                 })
@@ -110,7 +110,7 @@ describe('users.js', function() {
                         assert.equal(res.username, 'nick');
                         done();
                     } catch (err) {
-                        done(err);
+                        return done(err);
                     }
                 })
                 .catch((err) => done(err));
@@ -122,7 +122,7 @@ describe('users.js', function() {
                     try {
                         assert.equal(user.username, 'nick');
                     } catch (err) {
-                        done(err);
+                        return done(err);
                     }
                     return users.find('2');
                 })
@@ -131,7 +131,7 @@ describe('users.js', function() {
                         assert.equal(res, undefined);
                         done();
                     } catch (err) {
-                        done(err);
+                        return done(err);
                     }
                 })
                 .catch((err) => {
@@ -147,7 +147,7 @@ describe('users.js', function() {
                 .then((res) => {
                     const rows = res[0];
                     if (rows.length > 0) {
-                        done(new Error('user doesn\'t have 0 drinks in db'));
+                        return done(new Error('user doesn\'t have 0 drinks in db'));
                     }
                     return user.drinkBooze(12347, 'kalja');
                 })
@@ -155,10 +155,10 @@ describe('users.js', function() {
                 .then((res) => {
                     const rows = res[0];
                     if (rows.length !== 1) {
-                        done(new Error('user doesn\'t have 1 drinks in db'));
+                        return done(new Error('user doesn\'t have 1 drinks in db'));
                     }
                     if (rows[0].alcohol !== 12347) {
-                        done(new Error('invalid amount of alcohol in db'));
+                        return done(new Error('invalid amount of alcohol in db'));
                     }
                     done();
                 })
@@ -178,7 +178,7 @@ describe('users.js', function() {
                         assert.equal(rows[0].alcohol, 12347);
                         done();
                     } catch (err) {
-                        done(err);
+                        return done(err);
                     }
                 })
                 .catch((err) => {
@@ -197,7 +197,7 @@ describe('users.js', function() {
                         assert.equal(rows[0].alcohol, 12347);
                         done();
                     } catch (err) {
-                        done(err);
+                        return done(err);
                     }
                 })
                 .catch((err) => {
@@ -212,10 +212,10 @@ describe('users.js', function() {
             user.getDrinkSumForXHours(24)
                 .then((res) => {
                     try {
-                        assert.equal(res.sum, 12347*2);
+                        assert.equal(res.sum, 12347 * 2);
                         done();
                     } catch (err) {
-                        done(err);
+                        return done(err);
                     }
                 }).catch((err) => {
                     done(err);
@@ -233,7 +233,7 @@ describe('users.js', function() {
                         assert.equal(rows.length, 3);
                         assert(rows.find(x => x.description === 'testUndo'));
                     } catch (err) {
-                        done(err);
+                        return done(err);
                     }
                     return user.undoDrink();
                 })
@@ -244,7 +244,7 @@ describe('users.js', function() {
                         assert(!rows.find(x => x.description === 'testUndo'));
                         done();
                     } catch (err) {
-                        done(err);
+                        return done(err);
                     }
                 })
                 .catch((err) => {
@@ -255,18 +255,17 @@ describe('users.js', function() {
     });
 
     describe('User.joinGroup()', function() {
-        it('should insert user to a group in db', function(done) {
+        it('should insert user to a group in db as a hash of the groupid', function(done) {
             const user = blakkistest.users[0];
             user.joinGroup(12347)
                 .then(() => query('select * from users_in_groups where userid=$1', [user.userId]))
                 .then((res) => {
                     let rows = res[0];
-                    console.log(rows);
                     try {
                         assert(rows.find(x => x.userid === user.userId && x.groupid === utils.hashSha256(12347)));
                         done();
                     } catch (err) {
-                        done(err);
+                        return done(err);
                     }
                     return user.undoDrink();
                 })
@@ -274,6 +273,238 @@ describe('users.js', function() {
                     console.log(err);
                     done(err);
                 });
+        });
+    });
+
+    describe('User.drinkBoozeReturnEBAC()', function() {
+        it('should insert drink to the db and return ebac after that', function(done) {
+            const user = blakkistest.users[3];
+            user.getBooze()
+                .then((rows) => {
+                    try {
+                        assert.equal(rows.length, 0);
+                    } catch (err) {
+                        return done(err);
+                    }
+                    return user.drinkBoozeReturnEBAC(12347, 'kalja');
+                })
+                .then((ebac) => {
+                    try {
+                        assert(ebac.permilles > 0);
+                        assert(ebac.permilles30Min > 0);
+                        assert(ebac.grams > 0);
+                    } catch (err) {
+                        return done(err);
+                    }
+                    return user.getBooze();
+                })
+                .then((rows) => {
+                    try {
+                        assert.equal(rows.length, 1);
+                        done();
+                    } catch (err) {
+                        return done(err);
+                    }
+                })
+                .catch((err) => {
+                    console.log(err);
+                    done(err);
+                });
+        });
+    });
+
+    describe('User.updateInfo', function() {
+        it('should update user info in db', function(done) {
+            const user = blakkistest.users[0];
+            query('select * from users where userId=$1', [user.userId])
+                .then((res) => {
+                    const found = res[0][0];
+                    try {
+                        assert.equal(utils.decrypt(found.nick), '0');
+                        assert.equal(found.weight, 80);
+                        assert.equal(found.gender, 'mies');
+                        assert.equal(found.height, 180);
+                        assert.equal(found.read_terms, true);
+                    } catch (err) {
+                        return done(err);
+                    }
+                    return user.updateInfo('nick', 90, 'nainen', 200, false);
+                })
+                .then(() => query('select * from users where userId=$1', [user.userId]))
+                .then((res) => {
+                    const found = res[0][0];
+                    try {
+                        assert.equal(utils.decrypt(found.nick), 'nick');
+                        assert.equal(found.weight, 90);
+                        assert.equal(found.gender, 'nainen');
+                        assert.equal(found.height, 200);
+                        assert.equal(found.read_terms, false);
+                        done();
+                    } catch (err) {
+                        return done(err);
+                    }
+                })
+                .catch((err) => done(err));
+        });
+    });
+
+    describe('User.updateReadAnnouncements', function() {
+        it('should update user read announcements value in db', function(done) {
+            const user = blakkistest.users[0];
+            query('select * from users where userId=$1', [user.userId])
+                .then((res) => {
+                    const found = res[0][0];
+                    try {
+                        assert.equal(found.read_announcements, 1);
+                    } catch (err) {
+                        return done(err);
+                    }
+                    return user.updateReadAnnouncements(2);
+                })
+                .then(() => query('select * from users where userId=$1', [user.userId]))
+                .then((res) => {
+                    const found = res[0][0];
+                    try {
+                        assert.equal(found.read_announcements, 2);
+                        done();
+                    } catch (err) {
+                        return done(err);
+                    }
+                })
+                .catch((err) => done(err));
+        });
+    });
+
+    describe('User.drinkBoozeLate', function() {
+        it('should insert 2 drinks with different timestamps to db and return EBAC', function(done) {
+            const user = blakkistest.users[3];
+            user.getBooze()
+                .then((rows) => {
+                    try {
+                        assert.equal(rows.length, 0);
+                    } catch (err) {
+                        return done(err);
+                    }
+                    return user.drinkBoozeLate([{
+                        mg: 12347,
+                        text: 'kalja'
+                    }, {
+                        mg: 12347,
+                        text: 'kalja2'
+                    }], 2);
+                })
+                .then((ebac) => {
+                    try {
+                        assert(ebac.permilles > 0);
+                        assert(ebac.permilles30Min > 0);
+                        assert(ebac.grams > 0);
+                    } catch (err) {
+                        return done(err);
+                    }
+                    return user.getBooze();
+                })
+                .then((rows) => {
+                    try {
+                        assert.equal(rows.length, 2);
+                        assert.notEqual(rows[0].created, rows[1].created);
+                        assert.equal(rows[0].alcohol, 12347);
+                        assert.equal(rows[1].alcohol, 12347);
+                        done();
+                    } catch (err) {
+                        return done(err);
+                    }
+                })
+                .catch((err) => done(err));
+        });
+    });
+
+    describe('User.getBoozeForLastHours', function() {
+        it('should return drinks only for last N hours', function(done) {
+            const user = blakkistest.users[0];
+            user.getBooze()
+                .then((rows) => {
+                    try {
+                        assert.equal(rows.length, 2);
+                    } catch (err) {
+                        return done(err);
+                    }
+                    // drinkBoozeLate to insert a drink 3 hours into the past
+                    return user.drinkBoozeLate([{
+                        mg: 12347,
+                        text: 'kalja'
+                    }], 3);
+                })
+                .then(() => user.getBooze())
+                .then((rows) => {
+                    try {
+                        assert.equal(rows.length, 3);
+                    } catch (err) {
+                        return done(err);
+                    }
+                    return user.getBoozeForLastHours(2);
+                })
+                .then((rows) => {
+                    try {
+                        assert.equal(rows.length, 2);
+                    } catch (err) {
+                        return done(err);
+                    }
+                    return user.getBoozeForLastHours(4);
+                })
+                .then((rows) => {
+                    try {
+                        assert.equal(rows.length, 3);
+                    } catch (err) {
+                        return done(err);
+                    }
+                    done();
+                })
+                .catch((err) => done(err));
+        });
+    });
+
+    describe('User.getDrinkSumForXHours', function() {
+        it('should return drink sum only for last N hours', function(done) {
+            const user = blakkistest.users[0];
+            user.getBooze()
+                .then((rows) => {
+                    try {
+                        assert.equal(rows.length, 2);
+                    } catch (err) {
+                        return done(err);
+                    }
+                    // drinkBoozeLate to insert a drink 3 hours into the past
+                    return user.drinkBoozeLate([{
+                        mg: 12347,
+                        text: 'kalja'
+                    }], 3);
+                })
+                .then(() => user.getBooze())
+                .then((rows) => {
+                    try {
+                        assert.equal(rows.length, 3);
+                    } catch (err) {
+                        return done(err);
+                    }
+                    return user.getDrinkSumForXHours(2);
+                })
+                .then((res) => {
+                    try {
+                        assert.equal(res.sum, 12347*2);
+                    } catch (err) {
+                        return done(err);
+                    }
+                    return user.getDrinkSumForXHours(4);
+                })
+                .then((res) => {
+                    try {
+                        assert.equal(res.sum, 12347*3);
+                    } catch (err) {
+                        return done(err);
+                    }
+                    done();
+                })
+                .catch((err) => done(err));
         });
     });
 });
