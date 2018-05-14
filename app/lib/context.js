1755afc761299a156ec852e6050a4a69455b17fc
Joonas Ulmanen <joonas.ulmanen@gmail.com>
2018-05-14 20:50:47 +0300

    Update context to use class keyword

--- app/lib/context.js
+++ app/lib/context.js
@@ -26,91 +26,74 @@
 const log = require('loglevel').getLogger('system');
 let contexts = module.exports = {};
 
-contexts.Context = function(cmd, msg) {
-    this.cmd = cmd;
-    this.msg = msg;
-    this.phase = 'start';
-    this.variables = {};
-};
-
-contexts.Context.prototype.privateReply = function(text) {
-    let self = this;
-    let options = {
-        'parse_mode': 'Markdown',
-        'reply_to_message_id': this.msg.message_id,
-        'reply_markup': {
-            'remove_keyboard': true
-        }
-    };
-    return self.msg.sendMessage(self.msg.from.id, text, options)
-        .then(() => {
-            log.debug('Sent ' + text + ' to ' + self.msg.from.username);
-            return Promise.resolve();
-        }).catch((err) => {
-            log.error('couldn\'t send private msg! Err: ' + err);
-            log.debug(err.stack);
-            return Promise.reject(err);
-        });
-};
-
-contexts.Context.prototype.privateReplyWithKeyboard = function(text, keyboardButtons) {
-    let self = this;
-    let options = {
-        'parse_mode': 'Markdown',
-        'reply_markup': {
-            'keyboard': keyboardButtons,
-            'resize_keyboard': true,
-            'one_time_keyboard': false
-        },
-        'reply_to_message_id': this.msg.message_id
-    };
-    return self.msg.sendMessage(self.msg.from.id, text, options)
-        .then(() => {
-            log.debug('Sent ' + text + ' to ' + self.msg.from.username);
-            return Promise.resolve();
-        }).catch((err) => {
-            log.error('couldn\'t send private msg! Err: ' + err);
-            log.debug(err.stack);
-            return Promise.reject(err);
-        });
-};
-
-contexts.Context.prototype.chatReply = function(text) {
-    let self = this;
-    return self.msg.sendMessage(self.msg.chat.id, text)
-        .then(() => {
-            log.debug('Sent ' + text + ' to chat ' + self.msg.chat.title);
-            return Promise.resolve();
-        }).catch((err) => {
-            log.error('couldn\'t send chat msg! Err: ' + err);
-            log.debug(err.stack);
-            return Promise.reject(err);
-        });
-};
-
-contexts.Context.prototype.photoReply = function(stream, caption) {
-    let self = this;
-    return self.msg.sendPhoto(self.msg.chat.id, stream, {
-            caption: caption
-        })
-        .then(() => {
-            log.debug('Sent a photo to chat ' + self.msg.chat.title);
-            return Promise.resolve();
-        }).catch((err) => {
-            log.error('couldn\'t send photo to chat! Err: ' + err);
-            log.debug(err.stack);
-            return Promise.reject(err);
-        });
-};
-
-contexts.Context.prototype.sendMessage = function(message) {
-    if (!message || !message.type || !message.text) {
-        return Promise.reject(new Error('invalid message object'));
+contexts.Context = class {
+    constructor(cmd, msg) {
+        this.cmd = cmd;
+        this.msg = msg;
+        this.phase = 'start';
+        this.variables = {};
     }
-    let self = this;
 
-    if (message.type === 'photo') {
-        return self.msg.sendPhoto(self.msg.chat.id, message.buffer, message.options)
+    privateReply(text) {
+        let self = this;
+        let options = {
+            'parse_mode': 'Markdown',
+            'reply_to_message_id': this.msg.message_id,
+            'reply_markup': {
+                'remove_keyboard': true
+            }
+        };
+        return self.msg.sendMessage(self.msg.from.id, text, options)
+            .then(() => {
+                log.debug('Sent ' + text + ' to ' + self.msg.from.username);
+                return Promise.resolve();
+            }).catch((err) => {
+                log.error('couldn\'t send private msg! Err: ' + err);
+                log.debug(err.stack);
+                return Promise.reject(err);
+            });
+    }
+
+    privateReplyWithKeyboard(text, keyboardButtons) {
+        let self = this;
+        let options = {
+            'parse_mode': 'Markdown',
+            'reply_markup': {
+                'keyboard': keyboardButtons,
+                'resize_keyboard': true,
+                'one_time_keyboard': false
+            },
+            'reply_to_message_id': this.msg.message_id
+        };
+        return self.msg.sendMessage(self.msg.from.id, text, options)
+            .then(() => {
+                log.debug('Sent ' + text + ' to ' + self.msg.from.username);
+                return Promise.resolve();
+            }).catch((err) => {
+                log.error('couldn\'t send private msg! Err: ' + err);
+                log.debug(err.stack);
+                return Promise.reject(err);
+            });
+    }
+
+    chatReply(text) {
+        let self = this;
+        return self.msg.sendMessage(self.msg.chat.id, text)
+            .then(() => {
+                log.debug('Sent ' + text + ' to chat ' + self.msg.chat.title);
+                return Promise.resolve();
+            }).catch((err) => {
+                log.error('couldn\'t send chat msg! Err: ' + err);
+                log.debug(err.stack);
+                return Promise.reject(err);
+            });
+    }
+
+    photoReply(stream, caption) {
+        let self = this;
+        return self.msg.sendPhoto(self.msg.chat.id, stream, {
+                caption: caption
+            })
             .then(() => {
                 log.debug('Sent a photo to chat ' + self.msg.chat.title);
                 return Promise.resolve();
@@ -119,42 +102,61 @@ contexts.Context.prototype.sendMessage = function(message) {
                 log.debug(err.stack);
                 return Promise.reject(err);
             });
-    } else {
-        let to = message.type === 'private_message' ? self.msg.from : self.msg.chat;
-        return self.msg.sendMessage(to.id, message.text, message.options)
-            .then(() => {
-                log.debug('Sent ' + message.text + ' to ' + to.title || to.username || to.first_name);
-                return Promise.resolve();
-            }).catch((err) => {
-                log.error('couldn\'t send msg! Err: ' + err);
-                log.debug(err.stack);
-                return Promise.reject(err);
-            });
     }
-};
 
-contexts.Context.prototype.storeVariable = function(key, value) {
-    this.variables[key] = value;
-};
+    sendMessage(message) {
+        if (!message || !message.type || !message.text) {
+            return Promise.reject(new Error('invalid message object'));
+        }
+        let self = this;
 
-contexts.Context.prototype.fetchVariable = function(key) {
-    return this.variables[key];
-};
+        if (message.type === 'photo') {
+            return self.msg.sendPhoto(self.msg.chat.id, message.buffer, message.options)
+                .then(() => {
+                    log.debug('Sent a photo to chat ' + self.msg.chat.title);
+                    return Promise.resolve();
+                }).catch((err) => {
+                    log.error('couldn\'t send photo to chat! Err: ' + err);
+                    log.debug(err.stack);
+                    return Promise.reject(err);
+                });
+        } else {
+            let to = message.type === 'private_message' ? self.msg.from : self.msg.chat;
+            return self.msg.sendMessage(to.id, message.text, message.options)
+                .then(() => {
+                    log.debug('Sent ' + message.text + ' to ' + to.title || to.username || to.first_name);
+                    return Promise.resolve();
+                }).catch((err) => {
+                    log.error('couldn\'t send msg! Err: ' + err);
+                    log.debug(err.stack);
+                    return Promise.reject(err);
+                });
+        }
+    }
 
-contexts.Context.prototype.toPhase = function(phase) {
-    this.phase = phase;
-};
+    storeVariable(key, value) {
+        this.variables[key] = value;
+    }
 
-contexts.Context.prototype.end = function() {
-    this.phase = -1;
-    this.variables = 0;
-    return Promise.resolve();
-};
+    fetchVariable(key) {
+        return this.variables[key];
+    }
 
-contexts.Context.prototype.isPrivateChat = function() {
-    return this.msg.chat.type === 'private';
-};
+    toPhase(phase) {
+        this.phase = phase;
+    }
 
-contexts.Context.prototype.hasEnded = function() {
-    return this.phase === -1;
+    end() {
+        this.phase = -1;
+        this.variables = 0;
+        return Promise.resolve();
+    }
+
+    isPrivateChat() {
+        return this.msg.chat.type === 'private';
+    }
+
+    hasEnded() {
+        return this.phase === -1;
+    }
 };
