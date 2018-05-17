65a4db2320c82bc7b15eddb6dffba17a4fe1636a
Joonas Ulmanen <joonas.ulmanen@gmail.com>
2018-05-17 21:47:15 +0300

    Convert to use classes

--- app/db/users.js
+++ app/db/users.js
@@ -26,7 +26,7 @@
 const pg = require('pg');
 const pool = new pg.Pool({
     connectionString: process.env.DATABASE_URL
-});;
+});
 const log = require('loglevel').getLogger('db');
 const utils = require('../lib/utils.js');
 const alcomath = require('../lib/alcomath.js');
@@ -35,20 +35,129 @@ const settings = require('../settings.js');
 
 let users = module.exports = {};
 
-function User(userId, username, weight, gender, height, read_terms, read_announcements, created) {
-    this.userId = userId;
-    this.username = username;
-    this.weight = weight;
-    this.gender = gender;
-    this.height = height;
-    this.read_terms = read_terms;
-    this.read_announcements = read_announcements;
-    this.created = new Date(created);
+class User {
+    constructor(userId, username, weight, gender, height, read_terms, read_announcements, created) {
+        this.userId = userId;
+        this.username = username;
+        this.weight = weight;
+        this.gender = gender;
+        this.height = height;
+        this.read_terms = read_terms;
+        this.read_announcements = read_announcements;
+        this.created = new Date(created);
+    }
+    isAdmin() {
+        return utils.hashSha256(settings.admin_id) === this.userId;
+    }
+
+    drinkBooze(amount, description) {
+        return pool.query('insert into users_drinks (userId, alcohol, description) values($1, $2, $3)', [this.userId, amount, description]);
+    }
+
+    getBooze() {
+        return pool.query('select alcohol, description, created from users_drinks where userId = $1 order by created asc', [this.userId])
+            .then((res) => {
+                return Promise.resolve(res.rows);
+            });
+    }
+
+    getDrinkSumForXHours(hours) {
+        let hoursAgo = utils.getDateMinusHours(hours);
+        return pool.query('select sum(alcohol) as sum, min(created) as created from users_drinks where userId = $1 and created > $2 ', [this.userId, hoursAgo])
+            .then((res) => {
+                return Promise.resolve(res.rows[0]);
+            });
+    }
+
+    undoDrink() {
+        return pool.query('delete from users_drinks where created=(select created from users_drinks where userid = $1 order by created desc limit 1)', [this.userId])
+            .then((res) => {
+                return Promise.resolve(res.rows);
+            });
+    }
+
+    getBoozeForLastHours(hours) {
+        let hoursAgo = utils.getDateMinusHours(hours);
+        return pool.query('select alcohol, description, created from users_drinks where userId = $1 and created > $2 order by created desc', [this.userId, hoursAgo.toISOString()])
+            .then((res) => {
+                return Promise.resolve(res.rows);
+            });
+    }
+
+    joinGroup(groupId) {
+        let groupIdHash = utils.hashSha256(groupId);
+        return pool.query('select userId, groupId from users_in_groups where userId=$1 and groupId=$2', [this.userId, groupIdHash])
+            .then((res) => {
+                const rows = res.rows;
+                if (rows.length > 0) {
+                    return true;
+                } else {
+                    return pool.query('insert into users_in_groups (userId, groupId) values ($1, $2)', [this.userId, groupIdHash]);
+                }
+            });
+    }
+
+    leaveGroup(groupId) {
+        let groupIdHash = utils.hashSha256(groupId);
+        return pool.query('select userId, groupId from users_in_groups where userId=$1 and groupId=$2', [this.userId, groupIdHash])
+            .then((res) => {
+                const rows = res.rows;
+                if (rows.length > 0) {
+                    return pool.query('delete from users_in_groups where userId=$1 and groupId=$2', [this.userId, groupIdHash]);
+                } else {
+                    return true;
+                }
+            });
+    }
+
+    drinkBoozeReturnEBAC(amount, description) {
+        let self = this;
+        return self.drinkBooze(amount, description)
+            .then(() => self.getBooze())
+            .then((drinks) => {
+                let ebac = alcomath.calculateEBACFromDrinks(self, drinks);
+                return ebac;
+            });
+    }
+
+    drinkBoozeLate(drinks, hours) {
+        let self = this;
+        log.debug('Drinking late');
+        let queries = [];
+        for (let i in drinks) {
+            log.debug(drinks[i].text, drinks[i].mg);
+            let drink = drinks[i];
+            let hoursAgo = utils.getDateMinusHours(hours - hours / Math.max(drinks.length - 1, 1) * i);
+            log.debug(hoursAgo);
+            queries.push(pool.query('insert into users_drinks (userId, alcohol, description, created) values($1, $2, $3, $4)', [this.userId, drink.mg, drink.text, hoursAgo]));
+        }
+        return Promise.all(queries)
+            .then(() => self.getBooze())
+            .then((drinks) => {
+                let ebac = alcomath.calculateEBACFromDrinks(self, drinks);
+                return ebac;
+            });
+    }
+
+    updateInfo(username, weight, gender, height, read_terms) {
+        let self = this;
+        username = utils.encrypt(username);
+        return pool.query('update users set nick=$1, weight=$2, gender=$3, height=$4, read_terms=$5 where userId=$6 returning userId, nick, weight, gender, read_terms, created', [username, weight, gender, height, read_terms, self.userId])
+            .then((res) => {
+                const found = res.rows[0];
+                return new User(found.userid, utils.decrypt(found.nick), found.weight, found.gender, found.height, found.read_terms, found.read_announcements, found.created);
+            });
+    }
+
+    updateReadAnnouncements(read_announcements_count) {
+        const self = this;
+        return pool.query('update users set read_announcements=$1 where userId=$2', [read_announcements_count, self.userId]);
+    }
 }
 
 users.User = User;
 
-users.new = function(userId, nick, weight, gender, height, read_terms) {
+users.new = (userId, nick, weight, gender, height, read_terms) => {
     const params = [utils.hashSha256(parseInt(userId, 10)), utils.encrypt(nick), parseInt(weight, 10), gender, parseInt(height, 10), read_terms, announcements.length];
 
     log.debug('Creating user... params:');
@@ -64,7 +173,7 @@ users.new = function(userId, nick, weight, gender, height, read_terms) {
         });
 };
 
-users.find = function find(userId) {
+users.find = (userId) => {
     userId = utils.hashSha256(userId);
     return pool.query('select userId, nick, weight, gender, height, read_terms, read_announcements, created from users where userId=$1', [userId])
         .then((res) => {
@@ -78,112 +187,4 @@ users.find = function find(userId) {
                 return null;
             }
         });
-};
-
-User.prototype.isAdmin = function() {
-    return utils.hashSha256(settings.admin_id) === this.userId;
-};
-
-User.prototype.drinkBooze = function(amount, description) {
-    return pool.query('insert into users_drinks (userId, alcohol, description) values($1, $2, $3)', [this.userId, amount, description]);
-};
-
-User.prototype.getBooze = function() {
-    return pool.query('select alcohol, description, created from users_drinks where userId = $1 order by created asc', [this.userId])
-        .then((res) => {
-            return Promise.resolve(res.rows);
-        });
-};
-
-User.prototype.getDrinkSumForXHours = function(hours) {
-    let hoursAgo = utils.getDateMinusHours(hours);
-    return pool.query('select sum(alcohol) as sum, min(created) as created from users_drinks where userId = $1 and created > $2 ', [this.userId, hoursAgo])
-        .then((res) => {
-            return Promise.resolve(res.rows[0]);
-        });
-};
-
-User.prototype.undoDrink = function() {
-    return pool.query('delete from users_drinks where created=(select created from users_drinks where userid = $1 order by created desc limit 1)', [this.userId])
-        .then((res) => {
-            return Promise.resolve(res.rows);
-        });
-};
-
-User.prototype.getBoozeForLastHours = function(hours) {
-    let hoursAgo = utils.getDateMinusHours(hours);
-    return pool.query('select alcohol, description, created from users_drinks where userId = $1 and created > $2 order by created desc', [this.userId, hoursAgo.toISOString()])
-        .then((res) => {
-            return Promise.resolve(res.rows);
-        });
-};
-
-User.prototype.joinGroup = function(groupId) {
-    let groupIdHash = utils.hashSha256(groupId);
-    return pool.query('select userId, groupId from users_in_groups where userId=$1 and groupId=$2', [this.userId, groupIdHash])
-        .then((res) => {
-            const rows = res.rows;
-            if (rows.length > 0) {
-                return true;
-            } else {
-                return pool.query('insert into users_in_groups (userId, groupId) values ($1, $2)', [this.userId, groupIdHash]);
-            }
-        });
-};
-
-User.prototype.leaveGroup = function(groupId) {
-    let groupIdHash = utils.hashSha256(groupId);
-    return pool.query('select userId, groupId from users_in_groups where userId=$1 and groupId=$2', [this.userId, groupIdHash])
-        .then((res) => {
-            const rows = res.rows;
-            if (rows.length > 0) {
-                return pool.query('delete from users_in_groups where userId=$1 and groupId=$2', [this.userId, groupIdHash]);
-            } else {
-                return true;
-            }
-        });
-};
-
-User.prototype.drinkBoozeReturnEBAC = function(amount, description) {
-    let self = this;
-    return self.drinkBooze(amount, description)
-        .then(() => self.getBooze())
-        .then((drinks) => {
-            let ebac = alcomath.calculateEBACFromDrinks(self, drinks);
-            return ebac;
-        });
-};
-
-User.prototype.drinkBoozeLate = function(drinks, hours) {
-    let self = this;
-    log.debug('Drinking late');
-    let queries = [];
-    for (let i in drinks) {
-        log.debug(drinks[i].text, drinks[i].mg);
-        let drink = drinks[i];
-        let hoursAgo = utils.getDateMinusHours(hours - hours / Math.max(drinks.length - 1, 1) * i);
-        log.debug(hoursAgo);
-        queries.push(pool.query('insert into users_drinks (userId, alcohol, description, created) values($1, $2, $3, $4)', [this.userId, drink.mg, drink.text, hoursAgo]));
-    }
-    return Promise.all(queries)
-        .then(() => self.getBooze())
-        .then((drinks) => {
-            let ebac = alcomath.calculateEBACFromDrinks(self, drinks);
-            return ebac;
-        });
-};
-
-User.prototype.updateInfo = function(username, weight, gender, height, read_terms) {
-    let self = this;
-    username = utils.encrypt(username);
-    return pool.query('update users set nick=$1, weight=$2, gender=$3, height=$4, read_terms=$5 where userId=$6 returning userId, nick, weight, gender, read_terms, created', [username, weight, gender, height, read_terms, self.userId])
-        .then((res) => {
-            const found = res.rows[0];
-            return new User(found.userid, utils.decrypt(found.nick), found.weight, found.gender, found.height, found.read_terms, found.read_announcements, found.created);
-        });
-};
-
-User.prototype.updateReadAnnouncements = function(read_announcements_count) {
-    const self = this;
-    return pool.query('update users set read_announcements=$1 where userId=$2', [read_announcements_count, self.userId]);
 };
