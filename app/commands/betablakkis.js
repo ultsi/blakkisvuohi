e17f1bf1fb1105539c67169810d926839f66a68b
Joonas Ulmanen <joonas.ulmanen@gmail.com>
2018-05-19 16:00:52 +0300

    Include account creation file

--- app/commands/betablakkis.js
+++ app/commands/betablakkis.js
@@ -85,7 +85,7 @@ const betablakkis = {
     _root: true,
     [str_beta.juo.button_text]: require('./inline/juo.js'),
     [str_beta.tunnus.button_text]: require('./inline/tunnus.js'),
-    [str_beta.luo_tunnus.button_text]: require('./inline/tunnus.js'),
+    [str_beta.luo_tunnus.button_text]: require('./inline/luo_tunnus.js'),
     'Tilastot': {
         _text: 'lol stats'
     },
